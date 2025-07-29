400613863a828892bab9fd0217f02bf5b2deaaf8
Yurii <brukhyura2@gmail.com>
2025-07-29 14:29:10 +0200

    small grammar

--- backend/realworld/app.py
+++ backend/realworld/app.py
@@ -24,7 +24,7 @@ def _register_blueprints(app: Flask):
         tags_blueprint, url_prefix=f"/api{tags_blueprint.url_prefix}"
     )
 
-    @app.route("/api/health")
+    @app.route("/health")
     def health():
         return "ok", 200
 
