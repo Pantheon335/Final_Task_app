6d6ddad4de5b55452e08e843ce37ac95f58e9bcd
Yurii <brukhyura2@gmail.com>
2025-07-30 00:20:10 +0200

    general cleanup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,14 +2,10 @@
 
 set -e
 
-echo "Downloading environment file from S3..."
 aws s3 cp s3://brukhy-terraform-state/configs/backend.env /tmp/backend.env
 
-echo "Exporting variables from backend.env..."
 export $(grep -v '^#' /tmp/backend.env | xargs)
 
-echo "Running Alembic migrations..."
 poetry run alembic upgrade head
 
-echo "Starting Flask application..."
 exec flask run --host=0.0.0.0 --port=5000
